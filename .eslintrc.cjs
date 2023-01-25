defb34cc1d4c39dcdd990a8fea499edb51f817da
TsubasaHiga <info@higatsubasa.com>
2023-01-25 20:18:52 +0900

    update:#1 Add Lint tools eslint, prettier, react, husky, lint-staged

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    'plugin:astro/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'react-app',
+    'prettier'
+  ],
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      }
+    }
+  ]
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'none',
+  endOfLine: 'lf'
+}
