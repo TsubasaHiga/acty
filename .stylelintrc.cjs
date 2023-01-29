b40c1c4e39bce2f5d2144e06e236a36dc6a06d0d
TsubasaHiga <info@higatsubasa.com>
2023-01-29 20:19:59 +0900

    update:#1 sassとstylelintの導入

---
+++ .stylelintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier-scss',
+    'stylelint-config-html'
+  ],
+  rules: {
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'apply',
+          'each',
+          'else',
+          'extend',
+          'for',
+          'forward',
+          'function',
+          'if',
+          'include',
+          'layer',
+          'mixin',
+          'responsive',
+          'return',
+          'screen',
+          'tailwind',
+          'use'
+        ]
+      }
+    ]
+  }
+}
