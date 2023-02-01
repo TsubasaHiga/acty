1980d9dc1ace82294d6f221b15b3433350da6763
TsubasaHiga <info@higatsubasa.com>
2023-02-01 22:20:11 +0900

    update:#1 npx astro add react実施

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
