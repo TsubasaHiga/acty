a515a908ab49a26c399e8ae8025b407c8275b40a
TsubasaHiga <info@higatsubasa.com>
2023-02-02 23:03:35 +0900

    update:#1 Update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
 module.exports = {
-  extends: ['plugin:astro/recommended', 'plugin:tailwindcss/recommended', 'eslint:recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier'
+  ],
   plugins: ['simple-import-sort'],
   env: {
     browser: true,
     node: true
   },
   parserOptions: {
-    project: './tsconfig.json',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
@@ -15,27 +19,39 @@ module.exports = {
     'tailwindcss/no-custom-classname': 'off',
 
     // simple-import-sort/importsを有効化
-    'simple-import-sort/imports': 'error'
+    'simple-import-sort/imports': 'error',
+    // simple-import-sort/exportsを有効化
+    'simple-import-sort/exports': 'error'
   },
   overrides: [
+    // .astroファイルに対する設定
     {
       files: ['**/*.astro'],
+      extends: ['plugin:astro/recommended'],
       parser: 'astro-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro']
       }
     },
+
+    // .ts,.tsxファイルに対する設定
     {
       files: ['**/*.ts', '**/*.tsx'],
-      extends: ['plugin:@typescript-eslint/recommended', 'react-app'],
+      extends: ['react-app'],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
       rules: {
         // react/jsx-sort-propsを有効化
         'react/jsx-sort-props': [2],
 
         // react/jsx-uses-reactとreact/react-in-jsx-scopeを無効化
         'react/jsx-uses-react': 'off',
-        'react/react-in-jsx-scope': 'off'
+        'react/react-in-jsx-scope': 'off',
+
+        // @typescript-eslint/no-explicit-anyを無効化
+        '@typescript-eslint/no-explicit-any': 'off'
       }
     }
   ]
