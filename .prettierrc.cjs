e5fb5cf7878008634a38fc5340955130da6cbf00
TsubasaHiga <info@higatsubasa.com>
2023-02-06 22:05:26 +0900

    chore: ✨ #1 prettier-plugin-md-nocjspを追加

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,5 +5,20 @@ module.exports = {
   tabWidth: 2,
   printWidth: 120,
   trailingComma: 'none',
-  endOfLine: 'lf'
+  endOfLine: 'lf',
+  plugins: [require('prettier-plugin-md-nocjsp')],
+  overrides: [
+    {
+      files: ['*.md', 'README'],
+      options: {
+        parser: 'markdown-nocjsp'
+      }
+    },
+    {
+      files: '*.mdx',
+      options: {
+        parser: 'mdx-nocjsp'
+      }
+    }
+  ]
 }
