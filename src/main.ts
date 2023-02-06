ac850aad3b6e42a51a9e7acd5ba7d4d3a47f32ea
TsubasaHiga <info@higatsubasa.com>
2023-02-06 23:06:16 +0900

    chore: 🔨 共通スクリプトを読み込み

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import { debounce, throttle } from 'throttle-debounce'
+
+import AddUaData from '~/modules/AddUaData'
+import SetOrientation from '~/modules/SetOrientation'
+import GetDocumentH from '~/utils/getDocumentHeight'
+import Set100vh from '~/utils/set100vh'
+
+const onDOMContentLoaded = () => {
+  // AddUaData
+  new AddUaData()
+}
+
+const onLoad = () => {
+  Set100vh()
+  Set100vh('--vh-always')
+
+  // SetOrientation
+  new SetOrientation()
+
+  // onScroll
+  onScroll()
+
+  document.documentElement.classList.add('is-loaded')
+}
+
+const onScroll = () => {
+  const y = Math.round(window.scrollY || window.pageYOffset)
+
+  // add className is-scroll
+  y > 0 ? document.documentElement.classList.add('is-scroll') : document.documentElement.classList.remove('is-scroll')
+
+  // add className is-footer
+  GetDocumentH() <= y
+    ? document.documentElement.classList.add('is-footer')
+    : document.documentElement.classList.remove('is-footer')
+}
+
+let oldInnerWidth = window.innerWidth
+const onResize = () => {
+  Set100vh('--vh-always')
+
+  // window幅が変わった時
+  if (oldInnerWidth !== window.innerWidth) {
+    Set100vh()
+    oldInnerWidth = window.innerWidth
+  }
+}
+
+// addEventListeners
+window.addEventListener('DOMContentLoaded', onDOMContentLoaded)
+window.addEventListener('load', onLoad)
+window.addEventListener('scroll', throttle(100, onScroll), false)
+window.addEventListener('resize', debounce(100, onResize), false)
